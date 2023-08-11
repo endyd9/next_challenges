d620aebd21a77b17324fa834f97089aad24e2987
endyd9 <endyd9@gmail.com>
2023-08-12 03:48:53 +0900

    set refreshInterval

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ export default function App({ Component, pageProps }: any) {
       value={{
         fetcher: (url: string) =>
           fetch(url).then((response) => response.json()),
+        refreshInterval: 2000,
       }}
     >
       <div className="w-full max-w-xl mx-auto">
